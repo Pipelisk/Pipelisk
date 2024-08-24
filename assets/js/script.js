a861d0c5ee8caff89f77d30136c685742c652e1c
Felipe <FELIPE@DESKTOP-H2CEV7J>
2024-08-23 23:56:06 -0400

    ultimo commit antes de entregar

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById('btn1');
+boton.addEventListened('click', function() {
+    boton.style.backgroundColor = 'green';
+})
+
+function msj1() {
+    document.write('Consulta Enviada')
+}
